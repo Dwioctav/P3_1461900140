1f1bd1a33b831e3198cbfc08378c8462ffc4eed7
dwioctaviana <dwioctaviaana@gmail.com>
2021-06-05 14:56:01 +0700

    praktikum3

---
+++ app/Models/kamar.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kamar extends Model
+{
+    use HasFactory;
+    protected $table = "kamar";
+    protected $primarkey = "id";
+    protected $keyType = "char";
+
+    protected $fillable = [
+        'id',
+        'id_pasien',
+        'id_dokter'
+    ];
+}
---
+++ app/Models/pasien.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pasien extends Model
+{
+    use HasFactory;
+    protected $table = "pasien";
+    protected $primarkey = "id";
+    protected $keyType = "char";
+
+    protected $fillable = [
+        'id',
+        'nama',
+        'alamat'
+    ];
+}
